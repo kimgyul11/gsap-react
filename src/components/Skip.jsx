07fb74be61e4bada2d2de7b57f3107a49e995638
kimgyul11 <coloo88@naver.com>
2023-09-13 21:00:51 +0900

    리액트버전-헤더완료

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Skip = () => {
-  return <div>skip</div>;
+  return (
+    <div id="skip">
+      <a href="#header">헤더영역 바로가기</a>
+      <a href="#intro">소개영역 바로가기</a>
+      <a href="#skills">스킬영역 바로가기</a>
+      <a href="#site">사이트영역 바로가기</a>
+      <a href="#port">포트폴리오 바로가기</a>
+      <a href="#contact">연락처 바로가기</a>
+      <a href="#footer">연락처 바로가기</a>
+    </div>
+  );
 };
 
 export default Skip;
